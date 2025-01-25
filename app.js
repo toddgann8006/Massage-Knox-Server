5ec3990de2fc3312d7440180bd8d3373eb4965c0
toddgann8006 <89031976+toddgann8006@users.noreply.github.com>
2025-01-25 18:53:55 -0500

    New Update

--- app.js
+++ app.js
@@ -48,8 +48,6 @@ app.set('view engine', 'pug');
 
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
